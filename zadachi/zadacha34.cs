b18da3445b51175d39a9641d19c8c5840d1e9f56
susanna <sus@gmail.com>
2023-03-15 10:56:10 +0300

    задача34

---
+++ zadachi/zadacha34.cs
@@ -0,0 +1,25 @@
+﻿/*Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+
+[345, 897, 568, 234] -> 2*/
+
+int[] array = randomArray(4); 
+Console.Write($"[{string.Join(",",array)}] -> {countOfEven(array)}"); 
+
+int[] randomArray(int size) { 
+ Random random = new Random(); 
+ int[] array = new int[size]; 
+ for(int i = 0; i < array.Length; i++) { 
+ array[i] = random.Next(100,1000); 
+    }
+ return array; 
+}
+
+int countOfEven(int[] array) { 
+ int count = 0; 
+    for(int i = 0; i < array.Length; i++) {
+ if(array[i] % 2 == 0) { 
+ count++; 
+        }
+    }
+ return count; 
+}
